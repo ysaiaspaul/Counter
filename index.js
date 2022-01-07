ab7c549a672219555b9d333c4952a15617069bf3
Ysaias Paul <ysaiaspaul@gmail.com>
2022-01-06 23:07:29 -0500

    Counter. Updated the UI and added some functionality and logic to the buttons.

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 
+
+//Declare global variables
 let count = 0
 let countEl =  document.getElementById("count-el")
 
-
+//Increase counter
  function increment(){
      count = count + 1
 
      countEl.innerText = count
-     console.log(count)
  }
 
-function decrement(){
+//Decrease the count
 
+function decrement(){
+    if (count < 1){
+
+       count === 0
+
+    }else{
         count = --count
         countEl.innerText = count
-        console.log(count)
-
+    }
 }
+
+//Reset the counter
+function reset(){
+
+    if (count === 0){
+        //alert("Is already at 0!")
+        document.getElementById("alert").style.display = "block";
+    }else{
+        count = 0
+        countEl.innerText = count
+    }
+}
+//Close the alert message
+
+document.getElementById("closebtn").onclick = function() {closebtn()};
+
+function closebtn(){
+    document.getElementById("alert").style.display = "none";
+}
+
 
 
 
